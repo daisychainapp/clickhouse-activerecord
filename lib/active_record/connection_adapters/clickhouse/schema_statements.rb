3c791fbc9a3d8acb9bfb4bd0db6411c85ef0453d
Nathan Woodhull <woodhull@gmail.com>
2024-10-16 22:21:45 -0400

    Add EOFError retries to the clickhouse gem

--- lib/active_record/connection_adapters/clickhouse/schema_statements.rb
+++ lib/active_record/connection_adapters/clickhouse/schema_statements.rb
@@ -111,7 +111,12 @@ module ActiveRecord
 
         def do_execute(sql, name = nil, format: DEFAULT_RESPONSE_FORMAT, settings: {})
           log(sql, "#{adapter_name} #{name}") do
-            res = request(sql, format, settings)
+            retries = 2
+            begin
+              res = request(sql, format, settings)
+            rescue EOFError
+              retry if (retries -= 1) > 0 # rubocop:disable Style/NumericPredicate
+            end
             process_response(res, format, sql)
           end
         end
